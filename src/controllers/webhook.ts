3c45be2224497fb9e6b31c369bb9a25e6761d572
pengdst <pengkuh.dst@gmail.com>
2023-04-28 20:48:30 +0700

    feat: upsert client to db when receive appcenter webhook

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -1,24 +1,34 @@
 import {Request, Response} from 'express';
+import {Client} from "../models/clients.model";
 
 type AppCenterPayload = {
-  app_code: string
-  app_secret: string
-  app_name: string
-  admin_email: string
-  admin_token: string
-  qiscus_sdk_token: string
+    app_code: string
+    app_secret: string
+    app_name: string
+    admin_email: string
+    admin_token: string
+    qiscus_sdk_token: string
 }
 
 type AppCenterReq = {
-  body: AppCenterPayload
+    body: AppCenterPayload
 } & Request
 
 export class WebhookController {
-  AppCenter = (req: AppCenterReq, resp: Response) => {
-    // TODO: save to DB or something
-
-    return resp.status(200).send({
-      status: "ok"
-    })
-  };
+    AppCenter = async (req: AppCenterReq, resp: Response) => {
+        // TODO: save to DB or something
+        try {
+            const newClient = await Client.upsert(req.body)
+            return resp.status(200).send({
+                status: "ok",
+                data: newClient,
+            })
+        } catch (e: any) {
+            console.log(e)
+            return resp.status(500).send({
+                message: "internal server error",
+                reasons: e.message
+            })
+        }
+    };
 }
